d540cbe608e87c4d1a26b9367d8447678b130816
Keng Son <cheongks2@gmail.com>
2022-02-27 16:56:30 +0800

    Added delete button and let user to edit items

--- lib/AddItem.dart
+++ lib/AddItem.dart
@@ -14,7 +14,10 @@ import 'package:path/path.dart' as Path;
 import 'package:dotted_border/dotted_border.dart';
 
 class AddItem extends StatefulWidget {
-  const AddItem({Key? key}) : super(key: key);
+  final bool _isEdit;
+  final String _itemId;
+
+  const AddItem(this._isEdit, this._itemId, {Key? key}) : super(key: key);
 
   @override
   _AddItemState createState() => _AddItemState();
@@ -26,17 +29,39 @@ class _AddItemState extends State<AddItem> {
   final _formKey = GlobalKey<FormState>();
   User? user = FirebaseAuth.instance.currentUser;
   File? _imageFile;
-  String? imageUrl;
+  late String imageUrl;
+  DocumentSnapshot? groceryItem;
 
   TextEditingController _itemNameController = TextEditingController();
   TextEditingController _itemDescriptionController = TextEditingController();
   TextEditingController _itemStockController = TextEditingController();
   TextEditingController _itemPriceController = TextEditingController();
   TextEditingController _itemMeasurementController = TextEditingController();
-
   @override
   void initState() {
     super.initState();
+    fetchItemId();
+  }
+
+  fetchItemId() async {
+    DocumentReference itemId = FirebaseFirestore.instance
+        .collection('Items')
+        .doc(user!.uid)
+        .collection('Item')
+        .doc(widget._itemId);
+    if (widget._isEdit) {
+      await itemId.get().then((docs) {
+        setState(() {
+          _itemNameController.text = docs["itemName"];
+          _imageFile = File(docs["itemImage"]);
+          imageUrl = docs["itemImage"];
+          _itemDescriptionController.text = docs["itemDescription"];
+          _itemStockController.text = docs["stockAmount"];
+          _itemPriceController.text = docs["price"];
+          _itemMeasurementController.text = docs["measurementMatrix"];
+        });
+      });
+    }
   }
 
   Future pickImage(ImageSource source) async {
@@ -123,6 +148,55 @@ class _AddItemState extends State<AddItem> {
     );
   }
 
+  showDeleteItemsDialog() async {
+    return showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Remove Image'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text(
+                    'Are you sure to delete this item? Once deleted, data of this item will be no longer available.'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.black),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Ok',
+                style: TextStyle(color: Color(0xff2C6846)),
+              ),
+              onPressed: () async {
+                await FirebaseFirestore.instance
+                    .collection('Items')
+                    .doc(user!.uid)
+                    .collection('Item')
+                    .doc(widget._itemId)
+                    .delete();
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) => HomePage()));
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final isKeyboard = MediaQuery.of(context).viewInsets.bottom != 0;
@@ -131,8 +205,31 @@ class _AddItemState extends State<AddItem> {
       appBar: !isKeyboard
           ? AppBar(
               toolbarHeight: 65.0,
-              backgroundColor: Color(0xff2C6846),
-              title: const Text('Item Details'),
+              iconTheme: IconThemeData(
+                color: Color(0xff2C6846), //change your color here
+              ),
+              title: const Text('Item Details',
+                  style: TextStyle(color: Colors.black)),
+              elevation: 0,
+              backgroundColor: Colors.white,
+              actions: <Widget>[
+                MaterialButton(
+                  onPressed: () => {showDeleteItemsDialog()},
+                  child: Row(children: <Widget>[
+                    Icon(Icons.delete_forever, size: 30, color: Colors.red),
+                    Center(
+                        child: Padding(
+                      padding: EdgeInsets.fromLTRB(5, 0, 3, 0),
+                      child: Text("Delete Forever",
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 15),
+                          textAlign: TextAlign.center),
+                    ))
+                  ]),
+                )
+              ],
             )
           : null,
       body: Center(
@@ -179,29 +276,36 @@ class _AddItemState extends State<AddItem> {
                                   DottedBorder(
                                     padding: EdgeInsets.all(10.0),
                                     color: Color(0xff2C6846),
-                                    child: Image.file(_imageFile!,
-                                        width: 100,
-                                        height: 100,
-                                        fit: BoxFit.cover),
+                                    child: (widget._isEdit == true
+                                        ? Image.network(imageUrl,
+                                            width: 100,
+                                            height: 100,
+                                            fit: BoxFit.cover)
+                                        : Image.file(_imageFile!,
+                                            width: 100,
+                                            height: 100,
+                                            fit: BoxFit.cover)),
                                     borderType: BorderType.RRect,
                                     radius: Radius.circular(5),
                                     dashPattern: [10, 5],
                                   ),
                                 ],
                               ),
-                              Align(
-                                  alignment: Alignment.topLeft,
-                                  child: CircleAvatar(
-                                    radius: 10,
-                                    backgroundColor: Colors.white,
-                                    child: IconButton(
-                                      padding: EdgeInsets.zero,
+                              (widget._isEdit == false
+                                  ? Align(
                                       alignment: Alignment.topLeft,
-                                      icon: Icon(Icons.remove_circle),
-                                      color: Colors.red,
-                                      onPressed: () => {showMyDialog()},
-                                    ),
-                                  )),
+                                      child: CircleAvatar(
+                                        radius: 10,
+                                        backgroundColor: Colors.white,
+                                        child: IconButton(
+                                          padding: EdgeInsets.zero,
+                                          alignment: Alignment.topLeft,
+                                          icon: Icon(Icons.remove_circle),
+                                          color: Colors.red,
+                                          onPressed: () => {showMyDialog()},
+                                        ),
+                                      ))
+                                  : Container())
                             ],
                           ),
                         )
@@ -276,12 +380,24 @@ class _AddItemState extends State<AddItem> {
                             dashPattern: [10, 5],
                           ),
                         )),
+                  (widget._isEdit == true
+                      ? Container(
+                          margin: const EdgeInsets.fromLTRB(0, 8.0, 0, 0),
+                          child: Text(
+                              "* Changing image feature is yet to be available, stay tune!",
+                              style:
+                                  TextStyle(fontSize: 14.0, color: Colors.red)))
+                      : Container()),
                   SizedBox(height: 25),
-                  TextField(
+                  TextFormField(
                     keyboardType: TextInputType.multiline,
                     minLines: 4,
                     maxLines: null,
                     controller: _itemDescriptionController,
+                    validator: (input) {
+                      if (input!.isEmpty)
+                        return 'Description is required for every items';
+                    },
                     decoration: InputDecoration(
                         focusedErrorBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.red),
@@ -303,16 +419,14 @@ class _AddItemState extends State<AddItem> {
                   ),
                   SizedBox(height: 25),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       Expanded(
-                        flex: 1,
                         child: TextFormField(
                           keyboardType: TextInputType.number,
                           controller: _itemStockController,
                           validator: (input) {
                             if (input!.isEmpty)
-                              return 'Pleas enter Stock Amount for this item';
+                              return 'Stock Amount is required';
                           },
                           decoration: InputDecoration(
                             focusedErrorBorder: OutlineInputBorder(
@@ -341,8 +455,7 @@ class _AddItemState extends State<AddItem> {
                           keyboardType: TextInputType.number,
                           controller: _itemPriceController,
                           validator: (input) {
-                            if (input!.isEmpty)
-                              return 'Pleas enter the Price for this item';
+                            if (input!.isEmpty) return 'Item Price is required';
                           },
                           decoration: InputDecoration(
                             focusedErrorBorder: OutlineInputBorder(
@@ -406,23 +519,41 @@ class _AddItemState extends State<AddItem> {
                 borderRadius: new BorderRadius.circular(5.0),
               ),
               onPressed: () async {
-                await FirebaseFirestore.instance
-                    .collection('Items')
-                    .doc(user!.uid)
-                    .collection('Item')
-                    .doc()
-                    .set({
-                  "itemName": _itemNameController.text,
-                  "itemImage": imageUrl,
-                  "itemDescription": _itemDescriptionController.text,
-                  "stockAmount": _itemStockController.text,
-                  "price": "RM" + _itemPriceController.text,
-                  "measurementMatrix": _itemMeasurementController.text
-                });
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => HomePage()));
+                if (_formKey.currentState!.validate()) {
+                  if (widget._isEdit == true) {
+                    await FirebaseFirestore.instance
+                        .collection('Items')
+                        .doc(user!.uid)
+                        .collection('Item')
+                        .doc(widget._itemId)
+                        .update({
+                      "itemName": _itemNameController.text,
+                      "itemDescription": _itemDescriptionController.text,
+                      "stockAmount": _itemStockController.text,
+                      "price": _itemPriceController.text,
+                      "measurementMatrix": _itemMeasurementController.text
+                    });
+                  } else {
+                    await FirebaseFirestore.instance
+                        .collection('Items')
+                        .doc(user!.uid)
+                        .collection('Item')
+                        .doc()
+                        .set({
+                      "itemName": _itemNameController.text,
+                      "itemImage": imageUrl,
+                      "itemDescription": _itemDescriptionController.text,
+                      "stockAmount": _itemStockController.text,
+                      "price": "RM" + _itemPriceController.text,
+                      "measurementMatrix": _itemMeasurementController.text
+                    });
+                  }
+
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => HomePage()));
+                }
               },
               child: Text('Save',
                   style: TextStyle(
